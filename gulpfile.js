15f482a6a39c1aaaca5c067d4a281690c31b3381
Marek Stopka <marek.stopka@perlur.cz>
2016-06-19 00:11:20 +0200

    Update Elixir and NPM dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var elixir = require('laravel-elixir');
+require('babel-preset-react');
+require("babel-polyfill");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,11 +13,23 @@ var elixir = require('laravel-elixir');
  |
  */
 
+elixir.config.js.babel.options.presets = [
+  "react",
+  "es2015"
+];
+
+/*
+var browserify = require('browserify');
+var browserifyES6Transpiler = require('browserify-es6-transpiler');
+browserify().require("./resources/js/index.js").transform(browserifyES6Transpiler).bundle();
+*/
+
 elixir(function(mix) {
   var bootstrapPath = 'node_modules/bootstrap-sass/assets';
   var jqueryPath = 'node_modules/jquery';
-  mix.sass('app.scss')
+  mix.browserify('app.js')
     .copy(jqueryPath + '/dist/jquery.min.js', 'public/js')
     .copy(bootstrapPath + '/fonts', 'public/fonts')
-    .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js');
+    .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js')
+    .sass('app.scss');
 });
